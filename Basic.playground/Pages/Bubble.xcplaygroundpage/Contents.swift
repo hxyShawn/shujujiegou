70c5b9c66a55824261d0f3b2069f71567dd40fa2
hexiaoyi <hexiaoyi@mifengkong.cn>
2019-09-11 20:52:02 +0800

    init

---
+++ Basic.playground/Pages/Bubble.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func Bubble(sortArray: [Int]) {
+    var temp = sortArray.count - 1
+    for i in 0..<temp {
+        print(temp)
+        if i == 2{
+            temp = 2
+        }
+    }
+}
+
+var temp = [1,2,4,8,17,14,12,14,10,11,12]
+print(temp)
+//Bubble(sortArray: temp)
+
+
---
+++ Basic.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+//func BubbleSort (Array : [Int]) -> Array<Int> {
+//    var tempArray = Array;
+//
+//    return tempArray;
+//}
